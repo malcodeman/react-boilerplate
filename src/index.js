e6f2f98f4ee058fcddfe2e8d1e81134e747a33e8
malcodeman <26983729+malcodeman@users.noreply.github.com>
2020-07-17 13:09:20 +0200

    Upgrades dependencies

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { render } from "react-dom";
 
-import App from "./features/App";
+import App from "./App";
 
 const MOUNT_NODE = document.getElementById("root");
 
